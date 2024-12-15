5bcfd5e2240da56523d77fa85ef1affee13de491
luxorif <76787236+luxorif@users.noreply.github.com>
2024-12-15 04:56:36 +0000

    Old Files Removed

--- Old/old/webhook_old.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-
-webhook_url = 'https://webhook.site/f154f35a-b2c8-45a0-b0d3-73eb02316499'
-data = { 'name': 'This is an example for webhook' }
-requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
